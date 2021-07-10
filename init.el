e125e9df10de642a5fb5e296baa14955cee5cc37
thatguilherme <86926777+thatguilherme@users.noreply.github.com>
2021-07-09 23:48:04 -0300

    Update init.el

--- init.el
+++ init.el
@@ -1,3 +1,8 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
 (menu-bar-mode -1)
 (toggle-scroll-bar -1)
 (tool-bar-mode -1)
+(ido-mode 1)
